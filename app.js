dcc5f285f1b4bcf9889ed4fe92de9db40386ed29
Alejandro Quiñones <alejandro.quinones@outlook.es>
2023-03-20 15:46:22 -0300

    cors

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ app.use(express.json({ limit: '25mb' }))
 app.use(express.static('public'))
 app.use((_, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*')
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
     next()
 })
 app.use('/', indexRoutes)
